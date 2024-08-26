b50a691277894e958eabe2b47a8a71f20b93182f
Rafael <rafael.glez@gmail.com>
2024-08-25 18:03:08 -0600

    Ejemplo de navegación

---
+++ TabBarController/FavViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavViewController.swift
+//  TabBarController
+//
+//  Created by Rafael Gonzalez on 24/08/24.
+//
+
+import UIKit
+
+class FavViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
